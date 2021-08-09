dfc7d4c963edc81302cca528adbed7567e6f77c2
Nick <rdding1123@gmail.com>
2021-08-09 14:56:16 +0800

    update 6

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ console.log('version => 2.0.0')
 console.log('version => 3.0.0')
 console.log('version => 4.0.0')
 console.log('version => 5.0.0')
+console.log('version => 6.0.0')
 
 
 // set route
